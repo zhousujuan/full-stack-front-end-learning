2cbcfa526b4e1445a38703ad958037269d78bcf2
zhousujuan <179997445@qq.com>
2022-03-24 09:58:46 +0800

    ts基础语法—面向对象编程

---
+++ TypeScript/code/02_基础语法/01.面向对象/app.js
@@ -0,0 +1,14 @@
+var Site = /** @class */ (function () {
+    function Site() {
+    }
+    Site.prototype.name = function () {
+        console.log("zhousujuan");
+    };
+    return Site;
+}());
+var obj = new Site();
+obj.name();
+/*
+以上实例定义了一个类Site，该类有一个方法name(),该方法在终端上输出字符串zhousujuan。
+new 关键字创建类的对象，该对象调用方法name().
+*/ 
---
+++ TypeScript/code/02_基础语法/01.面向对象/app.ts
@@ -0,0 +1,13 @@
+class Site  {
+    name():void {
+        console.log("zhousujuan");
+        
+    }
+}
+var obj = new Site();
+obj.name();
+
+/*
+以上实例定义了一个类Site，该类有一个方法name(),该方法在终端上输出字符串zhousujuan。
+new 关键字创建类的对象，该对象调用方法name().
+*/
